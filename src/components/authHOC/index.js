dc309d63761fedd803a761aa64b8a54465988a1d
xiaopeng <12405301@qq.com>
2018-04-08 19:14:52 +0800

    fix CORS

--- src/components/authHOC/index.js
+++ src/components/authHOC/index.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import { withRouter } from "react-router-dom";
+import React, {Component} from 'react';
+import {withRouter} from "react-router-dom";
 
 export default function author(WrappedComponent) {
-    class AuthInterceptor extends Component {
+  class AuthInterceptor extends Component {
 
-        componentWillMount() {
-            const isLoggedIn = !!window.localStorage.getItem("uid");
-            if(!isLoggedIn){
-                this.props.history.replace('/login');
-            }
-        }
-
-        render() {
-            return <WrappedComponent {...this.props} />;
-        }
+    componentWillMount() {
+      console.log(window.localStorage.getItem("uid"));
+      const isLoggedIn = !!window.localStorage.getItem("pt-uid");
+      if (!isLoggedIn) {
+        this.props.history.replace('/login');
+      }
     }
 
-    return withRouter(AuthInterceptor);
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+
+  return withRouter(AuthInterceptor);
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,61 +1,132 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import qs from 'qs';
 import {
-    Grid,
-    Row,
-    Button,
-    Form,
-    FormControl,
-    FormGroup,
-    Col,
-    ControlLabel,
-    Checkbox} from 'react-bootstrap';
+  Grid,
+  Row,
+  Button,
+  Form,
+  FormControl,
+  FormGroup,
+  Col,
+  ControlLabel,
+  Checkbox} from 'react-bootstrap';
 
 import './index.css';
 
 class Login extends Component {
-    render() {
-        return (
-            <Grid>
-                <Row>
-                    <Col xs={12} md={12}>
-                        <Form horizontal>
-                            <FormGroup controlId="formHorizontalEmail">
-                                <Col componentClass={ControlLabel} sm={2}>
-                                    邮件
-                                </Col>
-                                <Col sm={10}>
-                                    <FormControl type="email" placeholder="Email" />
-                                </Col>
-                            </FormGroup>
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      password: ''
+    };
 
-                            <FormGroup controlId="formHorizontalPassword">
-                                <Col componentClass={ControlLabel} sm={2}>
-                                    密码
-                                </Col>
-                                <Col sm={10}>
-                                    <FormControl type="password" placeholder="Password" />
-                                </Col>
-                            </FormGroup>
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-                            <FormGroup>
-                                <Col smOffset={2} sm={10}>
-                                    <Checkbox>记住我</Checkbox>
-                                </Col>
-                            </FormGroup>
+  handleInputChange(event) {
+    const target = event.target;
+    const name = target.name;
 
-                            <FormGroup>
-                                <Col smOffset={2} sm={10}>
-                                    <Button type="submit">
-                                        登录
-                                    </Button>
-                                </Col>
-                            </FormGroup>
-                        </Form>
-                    </Col>
-                </Row>
-            </Grid>
-        );
-    }
+    this.setState({
+      [name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // 添加请求拦截器
+    axios.interceptors.request.use(function (config) {
+      // 在发送请求之前做些什么
+      console.log('在发送请求之前做些什么');
+      // config.headers = {
+      //   'Content-Type': 'application/x-www-from-urlencoded'
+      // }
+
+      config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+      if(config.method === 'post') {
+        config.data = qs.stringify( {
+          ...config.data
+        })
+      }
+      console.log(config.data);
+      return config;
+    }, function (error) {
+      // 对请求错误做些什么
+      return Promise.reject(error);
+    });
+
+
+    axios.post('http://127.0.0.1:4000/api/login',{
+      name: this.state.name,
+      password: this.state.password
+    })
+    .then(function(res){
+      console.log(res);
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <Grid className="container-fluid">
+        <Row>
+          <Col md={4}/>
+          <Col md={4} className="vertical-center-col" >
+            <Form horizontal onSubmit={this.handleSubmit}>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} md={12}>
+                  账户
+                </Col>
+                <Col md={12}>
+                  <FormControl
+                    name="name"
+                    value={this.state.name}
+                    type="text"
+                    placeholder="Account"
+                    onChange={this.handleInputChange}/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <Col componentClass={ControlLabel} md={12}>
+                  密码
+                </Col>
+                <Col md={12}>
+                  <FormControl
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleInputChange}/>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col mdOffset={2} md={12}>
+                  <Checkbox> 记住我</Checkbox>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col mdOffset={2} md={12}>
+                  <Button bsStyle="primary" type="submit">
+                    登录
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col md={4}/>
+        </Row>
+      </Grid>
+    );
+  }
 }
 
 export default Login;
